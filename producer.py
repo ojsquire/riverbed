b1da3028d6ed27c39f2c2c49858a1d1986e18328
ojsquire <ojsquire@gmail.com>
2020-11-08 14:45:43 +0100

    Add dict data example

--- producer.py
+++ producer.py
@@ -1,10 +1,19 @@
 from kafka import KafkaProducer
+import json
 
-bootstrap_servers = ['localhost:9091', 'localhost:9092', 'localhost:9093']
-topicName = 'water3'
+bootstrap_servers = ["localhost:9091", "localhost:9092", "localhost:9093"]
+topicName = "water3"
 
 producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
 producer = KafkaProducer()
 
-producer.send(topicName, b'Hello World 2!')
+# String example
+# producer.send(topicName, b"Hello World 2!")
+
+# Dictionary example
+transaction = {"message": "who won the election?", "answer": "biden"}
+message = json.dumps(transaction)
+
+producer.send(topicName, value=message.encode())
+
 producer.flush()
